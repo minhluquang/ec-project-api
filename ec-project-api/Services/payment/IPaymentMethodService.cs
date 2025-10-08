bcf66dc5b86f92584cd100c8193a2212dd2edb19
minhtrii2810@gmail.com <minhtrii2810@gmail.com>
2025-10-08 14:09:33 +0700

    PaymentMethod

---
+++ ec-project-api/Services/payment/IPaymentMethodService.cs
@@ -0,0 +1,9 @@
+﻿using ec_project_api.Models;
+using ec_project_api.Services.Bases;
+
+namespace ec_project_api.Services.payment
+{
+    public interface IPaymentMethodService : IBaseService<PaymentMethod, int>
+    {
+    }
+}
---
+++ ec-project-api/Services/payment/PaymentService.cs
@@ -0,0 +1,13 @@
+﻿using ec_project_api.Interfaces;
+using ec_project_api.Models;
+using ec_project_api.Services.Bases;
+
+namespace ec_project_api.Services.payment
+{
+    public class PaymentService : BaseService<PaymentMethod, int>
+    {
+        public PaymentService(IRepository<PaymentMethod, int> repository) : base(repository)
+        {
+        }
+    }
+}
