6209d696eaccc23bc02fa3770f478d9e8166fdac
Minh LQ <luminh2004@gmail.com>
2025-10-13 21:51:29 +0700

    chore: product group attr

--- ec-project-api/Models/products/ProductGroup.cs
+++ ec-project-api/Models/products/ProductGroup.cs
@@ -12,7 +12,7 @@ namespace ec_project_api.Models {
         public string? Name { get; set; }
 
         [Column("status_id")]
-        public int StatusId { get; set; }
+        public short StatusId { get; set; }
 
         [Required]
         [Column("created_at")]
