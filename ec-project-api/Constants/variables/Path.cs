47c12abfd1d83538e53c41b1a3c314703937abbc
dien2107 <nguyenthanhdien910@gmail.com>
2025-10-13 20:45:43 +0700

    feat: lấy profile user

--- ec-project-api/Constants/variables/Path.cs
+++ ec-project-api/Constants/variables/Path.cs
@@ -76,5 +76,6 @@ namespace ec_project_api.Constants.variables {
         // ===============================
         public const string AssignRoles = "assign-roles";
         public const string ChangePassword = "change-password";
+        public const string UserProfile = "me";
     }
 }
