e063f1959e0f72820014f92d3f38961990be3169
dhuyhoang1406 <dhuyhoang181@gmail.com>
2025-10-24 13:19:58 +0700

    merge

--- ec-project-api/Constants/variables/Path.cs
+++ ec-project-api/Constants/variables/Path.cs
@@ -82,5 +82,6 @@ namespace ec_project_api.Constants.variables {
         public const string AssignRoles = "assign-roles";
         public const string ChangePassword = "change-password";
         public const string UserProfile = "me";
+        public const string VerifyCheck = "verify-successful";
     }
 }
