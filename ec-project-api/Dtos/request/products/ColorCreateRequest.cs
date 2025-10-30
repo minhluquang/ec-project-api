f9984cda564222c4536abdf4e210f79c201b9473
DHH.COG <110273797+HoangDang119763@users.noreply.github.com>
2025-10-31 00:43:21 +0700

    Fix size short

--- ec-project-api/Dtos/request/products/ColorCreateRequest.cs
+++ ec-project-api/Dtos/request/products/ColorCreateRequest.cs
@@ -8,9 +8,11 @@ namespace ec_project_api.Dtos.request.products
         [StringLength(50, ErrorMessage = "Tên màu sắc không được vượt quá 50 ký tự")]
         public string Name { get; set; } = null!;
 
+        [Required(ErrorMessage = "Vui lòng nhập tên hiển thị")]
         [StringLength(50, ErrorMessage = "Tên hiển thị không được vượt quá 50 ký tự")]
         public string? DisplayName { get; set; }
 
+        [Required(ErrorMessage = "Vui lòng nhập mã màu hex")]
         [StringLength(7, ErrorMessage = "Mã màu hex không hợp lệ")]
         public string? HexCode { get; set; }
     }
