bed5efc6c336e2f8a9928babf5c80a92fbe11893
dien2107 <126471154+dien2107@users.noreply.github.com>
2025-11-10 22:47:25 +0700

    Allow credentials in CORS policy for frontend

--- ec-project-api/Program.cs
+++ ec-project-api/Program.cs
@@ -252,6 +252,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins("http://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod()
+              .AllowCredentials()
     );
 });
 
